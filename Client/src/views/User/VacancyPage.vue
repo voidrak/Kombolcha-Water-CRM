9bc87cc86d35bd7ebaf32c9c58e41e4c42ff99d7
RohaTech <raklovetocode@gmail.com>
2025-02-21 10:45:53 +0300

    Add search functionality to VacancyPage component

--- Client/src/views/User/VacancyPage.vue
+++ Client/src/views/User/VacancyPage.vue
@@ -1,10 +1,11 @@
 <script setup>
 import GuestLayout from '@/layout/GuestLayout.vue';
 import { useVacancyStore } from '@/stores/vacancy';
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, reactive, ref } from 'vue';
 
 const { getAllVacancies } = useVacancyStore()
 const { deleteVacancy } = useVacancyStore()
+const searchQuery = ref('');
 
 
 const jobs = ref([]);
@@ -15,6 +16,16 @@ onMounted(async () => {
 })
 
 
+const filteredJobs = computed(() => {
+  if (!searchQuery.value) {
+    return jobs.value;
+  }
+  return jobs.value.filter(job =>
+    job.title.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
+    job.type.toString().includes(searchQuery.value) ||
+    job.location.toString().includes(searchQuery.value)
+  );
+});
 
 </script>
 
@@ -26,7 +37,23 @@ onMounted(async () => {
       <div v-if="jobs"
         class="relative flex flex-col gap-y-4 items-center justify-center overflow-hidden bg-gray-50 p-6 sm:py-12">
 
-        <div v-for="(job, index) in jobs" :key="index"
+        <div class=" pt-2 relative py-4 max-w-screen-md text-gray-600">
+          <input v-model="searchQuery"
+            class="border-2 w-full border-gray-300 bg-white h-10 py-2 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            type="search" name="search" placeholder="Search">
+          <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
+            <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
+              viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
+              width="512px" height="512px">
+              <path
+                d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+            </svg>
+          </button>
+        </div>
+
+
+        <div v-for="(job, index) in filteredJobs" :key="index"
           class="bg-white  shadow-xl shadow-gray-100 w-full max-w-4xl flex flex-col sm:flex-row gap-3 sm:items-center  justify-between px-5 py-4 rounded-md">
           <div>
 
